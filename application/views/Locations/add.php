d93d91ef020896e59a1552d8d5bd1c2ed15f470d
zainabzoaib <zainab.zoaib@gmail.com>
2017-03-01 20:38:54 +0500

    Created Locations Add and Edit Views

--- application/views/Locations/add.php
+++ application/views/Locations/add.php
@@ -1,9 +1,12 @@
 <?php $this->load->view('blocks/header'); ?>
 <?php $this->load->view('blocks/navigation'); ?>
+ <div class="container-fluid">
 <div class="col-md-9">
     <div class="panel panel-primary">
         <div class="panel-body">
-            <h4>Add New Location</h4>
+       <div class="container-fluid">
+<div class="col-md-12">
+<h4>Add New Location</h4>
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group">
@@ -21,7 +24,10 @@
                             <input type="checkbox" ng-model="showGoogleMap" class="iswitch iswitch-secondary ng-valid ng-dirty ng-valid-parse ng-not-modified ng-touched" ng-change="toggleMap(event)" id="if-map">
                             <label id="if-map" class="top-fix ng-scope" translate="">Include Map Coordinates for Location</label>
                         </div>
-                    </div>        
+                    </div>
+                    <div class="form-group">
+                        <?php echo form_submit("Save", "Save", array('class' => 'btn btn-primary pull-right')); ?>
+                    </div>
                         
                 </div>
             </div>
--- application/views/Locations/edit.php
+++ application/views/Locations/edit.php
@@ -1,89 +1,38 @@
 <?php $this->load->view('blocks/header'); ?>
 <?php $this->load->view('blocks/navigation'); ?>
-<div class="col-md-4">
-    <div class="panel panel-default panel-primary">
-        <div class="panel-body">
-            Panel content 1
-        </div>
-    </div>
-    <div class="panel panel-default panel-primary">
-        <div class="panel-body">
-            Panel content 2
-        </div>
-    </div>
-    <div class="panel panel-default panel-primary">
-        <div class="panel-body">
-            Panel content 3
-        </div>
-    </div>
-    <div class="panel panel-default panel-primary">
-        <div class="panel-body">
-            Panel content 4
-        </div>
-    </div>
-    <div class="panel panel-default panel-primary">
-        <div class="panel-body">
-            Panel content 5
-        </div>
-    </div>
-    <div class="row clearfix">
-        <div class="col-md-12 column">
-            <a href="<?php echo site_url('WorkOrders/add'); ?>" class="btn btn-primary">+</a>
-        </div>
-    </div>
-</div>
-<div class="col-md-5">
+ <div class="container-fluid">
+<div class="col-md-9">
     <div class="panel panel-primary">
         <div class="panel-body">
-            <h4>Add New Work Order</h4>
+       <div class="container-fluid">
+<div class="col-md-12">
+<h4>Add New Location</h4>
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group">
-                        <h4 class="control-label">Work Order Info</h4>
-                        <?php echo form_input('workorder_title', '', array('class' => 'form-control', 'placeholder' => 'Work order title*')) ?>
+                         <?php echo form_label('Name', '', array('class' => 'control-label')); ?>
+                    <div class="form-group">
+                        <?php echo form_input('Location_name', '', array('class' => 'form-control', 'placeholder' => 'Name of the Location*')) ?>
+                    </div>
+                        <div class="form-group">
+                         <?php echo form_label('Address', '', array('class' => 'control-label')); ?>
+                    <div class="form-group">
+                        <?php echo form_input('Location_address', '', array('class' => 'form-control', 'placeholder' => 'Address of the Location*')) ?>
                     </div>
                     <div class="form-group">
-                        <?php echo form_textarea('workorder_title', '', array('class' => 'form-control', 'placeholder' => 'Detailed description', 'rows' => '3')); ?>
-                    </div>
-                    <?php echo form_label('Set Priority', 'workorder_priority', array('class' => 'control-label')); ?>
-                    <div class="form-group">
-                        <label class="radio-inline">
-                            <?php echo form_radio('workorder_priority', '0', FALSE); ?> None
-                        </label>
-                        <label class="radio-inline">
-                            <?php echo form_radio('workorder_priority', '1', FALSE); ?> Low
-                        </label>
-                        <label class="radio-inline">
-                            <?php echo form_radio('workorder_priority', '2', FALSE); ?> Medium
-                        </label>
-                        <label class="radio-inline">
-                            <?php echo form_radio('workorder_priority', '3', FALSE); ?> High
-                        </label>
+                        <div class="form-block">
+                            <input type="checkbox" ng-model="showGoogleMap" class="iswitch iswitch-secondary ng-valid ng-dirty ng-valid-parse ng-not-modified ng-touched" ng-change="toggleMap(event)" id="if-map">
+                            <label id="if-map" class="top-fix ng-scope" translate="">Include Map Coordinates for Location</label>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <?php echo form_dropdown('workorder_location', array('Select Location', 'Location1', 'Location2', 'Loacation3'), FALSE, array('class' => 'form-control')) ?>
+                        <?php echo form_submit("Save", "Save", array('class' => 'btn btn-primary pull-right')); ?>
                     </div>
-                    <div class="form-group">
-                        <?php echo form_label('Assign Staff', 'workorder_priority', array('class' => 'control-label')); ?>
-                        <?php echo form_dropdown('workorder_worker', array('Select Main Worker', 'Location1', 'Location2', 'Loacation3'), FALSE, array('class' => 'form-control')) ?>
-                    </div>
-                    <div class="form-group">
-                        <?php echo form_dropdown('workorder_team', array('Select Team', 'Location1', 'Location2', 'Loacation3'), FALSE, array('class' => 'form-control')) ?>
-                    </div>
-                    <div class="form-group">
-                        <?php echo form_label('Assign Due Date', 'workorder_priority', array('class' => 'control-label')); ?>
-                        <?php echo form_input(array('type' => 'date', 'name' => 'workorder_duedate', 'class' => 'form-control')) ?>
-                    </div>
-                    <div class="form-group">
-                        <?php echo form_label('End Due Date', 'workorder_priority', array('class' => 'control-label')); ?>
-                        <?php echo form_input(array('type' => 'date', 'name' => 'workorder_end_duedate', 'class' => 'form-control')) ?>
-                    </div>
-                    <div class="form-group">
-                        <?php echo form_dropdown('workorder_repeating_schedule', array('Select Repeating Schedule', 'Location1', 'Location2', 'Loacation3'), FALSE, array('class' => 'form-control')) ?>
-                    </div>
+                        
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </div>
 <?php $this->load->view('blocks/footer'); ?>
