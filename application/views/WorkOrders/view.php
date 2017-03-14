346cff94543ac9485880eb34b5bc236dda947bab
zainabzoaib <zainab.zoaib@gmail.com>
2017-03-14 22:27:57 +0500

    view pura

--- application/views/WorkOrders/view.php
+++ application/views/WorkOrders/view.php
@@ -48,6 +48,7 @@
 <div class="col-md-6">
     <div class="panel panel-primary">
         <div class="panel-body">
+            <h3>Test Tasks</h3>
             <div class="row clearfix">
                 <div class="col-md-12 column">
                     <div class="tabbable" id="tabs-145895">
@@ -88,73 +89,160 @@
                                             </tr>
                                         </table>
 
-                                        <h3>Tasks</h3>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-repeat"></i></a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-warning btn-circle btn-lg"><i class="glyphicon glyphicon-stop"></i></a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-success btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-md-12 column">
+                        <div class="tabbable" id="tabs-145895">
+                            <ul class="nav nav-tabs">
+                                <li class="active">
+                                    <a href="#panel-168166" data-toggle="tab">Details</a>
+                                </li>
+                                <li>
+                                    <a href="#panel-403986" data-toggle="tab">Updates</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="panel-168166">
+                                    <div class="row clearfix">
+                                        <div class="col-md-12 column">
+                                            <h3>Location Details</h3>
+                                            <table class="table table-responsive table-bordered">
+                                                <tr>
+                                                    <td>Location Name</td>
+                                                    <td>Location Address </td>
+                                                </tr>
+                                            </table>
 
-                                        <table class="table table-responsive table-bordered">
-                                            <tr>
-                                                <td>Task Name 1</td>
-                                                <td>Comments Button info button</td>
-                                                <td>Done checkbox</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Task Name 2</td>
-                                                <td>Comments Button info button</td>
-                                                <td>Done Checkbox</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Task Name 3</td>
-                                                <td>Comments Button info button</td>
-                                                <td>Done Checkbox</td>
-                                            </tr>
-                                        </table>
-                                    </div>
+                                            <h3>Tasks</h3>
 
-                                    <div class="form-group">
-                                        <?php echo form_label('Assign Staff', 'workorder_priority', array('class' => 'control-label')); ?>
-                                        <?php echo form_dropdown('workorder_worker', set_value('workorder_worker', $workorder->category_id), TRUE, array('class' => 'form-control')) ?>
+                                            <table class="table table-responsive table-bordered">
+                                                <tr>
+                                                    <td>
+                                                        <table>
+                                                            <tr>
+                                                                <td>Task Name 1</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> <input type="button" value="Incomplete" onclick="">
+                                                                    <input type="button" value="On Hold" onclick="">
+                                                                    <input type="button" value="In Progress" onclick="">
+                                                                    <input type="button" value="Completed" onclick=""></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>User Assigned:</td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <table>
+                                                            <tr>
+                                                                <td>Task Name 2</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> <input type="button" value="Incomplete" onclick="">
+                                                                    <input type="button" value="On Hold" onclick="">
+                                                                    <input type="button" value="In Progress" onclick="">
+                                                                    <input type="button" value="Completed" onclick=""></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>User Assigned:</td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <table>
+                                                            <tr>
+                                                                <td>Task Name 3</td>
+                                                            </tr>
+                                                            <tr>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>User Assigned:</td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+
+                                            </table>
+                                            <h3>Work Orders Details</h3>
+                                            <table class="table table-responsive table-bordered">
+                                                <div class="col-md-12 column">
+                                                    <tr>
+                                                        <td>Word Order #:</td>
+                                                        <td>Due Date</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Created On:</td>
+                                                        <td>Created By:</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Assigned To:</td>
+                                                        <td>Last Updated:</td>
+                                                    </tr>
+                                            </table>
+                                            <h3>Time And Cost</h3>
+                                            <table class="table table-responsive table-bordered">
+                                                <div class="col-md-12 column">
+                                                    <tr>
+                                                        <td>Time:</td>
+                                                    </tr>
+                                                    <tr>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Cost:</td>
+                                                    </tr>
+                                            </table>
+                                        </div>
+                                        <div class="form-group">
+                                            <?php echo form_label('Requires Signature', 'workorder_requires_signature', array('class' => 'control-label')); ?>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="switch">
+                                                <input type="checkbox" name="workorder_requires_signature" <?php echo set_value('workorder_requires_signature', $workorder->requires_sign) == 1 ? 'checked' : '' ?>>
+                                                <div class="slider round"></div>
+                                            </label>
+                                        </div>
+                                        <div class="form-group">
+                                            <?php echo form_submit("Save", "Save", array('class' => 'btn btn-primary pull-right')); ?>
+                                        </div>
+                                        <?php echo form_close(); ?>
                                     </div>
-                                    <div class="form-group">
-                                        <?php echo form_dropdown('workorder_team', array('Select Team', 'Location1', 'Location2', 'Loacation3'), FALSE, array('class' => 'form-control')) ?>
-                                    </div>
-                                    <div class="form-group">
-                                        <?php echo form_label('Assign Due Date', 'workorder_priority', array('class' => 'control-label')); ?>
-                                        <?php echo form_input(array('type' => 'date', 'name' => 'workorder_duedate', 'value' => set_value('workorder_duedate', $workorder->start_date), 'class' => 'form-control')) ?>
-                                    </div>
-                                    <div class="form-group">
-                                        <?php echo form_label('End Due Date', 'workorder_priority', array('class' => 'control-label')); ?>
-                                        <?php echo form_input(array('type' => 'date', 'name' => 'workorder_end_duedate', 'value' => set_value('workorder_end_duedate', $workorder->end_date), 'class' => 'form-control')) ?>
-                                    </div>
-                                    <div class="form-group">
-                                        <?php echo form_dropdown('workorder_repeating_schedule', array('Select Repeating Schedule', '1' => 'Daily', '2' => 'Week days', '3' => 'Every Week', '4' => 'Every Two Weeks', '5' => 'Every Month', '6' => 'Every Year'), set_value('workorder_repeating_schedule', $workorder->repeating_schedule), array('class' => 'form-control')) ?>
-                                    </div>
-                                    <div class="form-group">
-                                        <?php echo form_label('Requires Signature', 'workorder_requires_signature', array('class' => 'control-label')); ?>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="switch">
-                                            <input type="checkbox" name="workorder_requires_signature" <?php echo set_value('workorder_requires_signature', $workorder->requires_sign) == 1 ? 'checked' : '' ?>>
-                                            <div class="slider round"></div>
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <?php echo form_submit("Save", "Save", array('class' => 'btn btn-primary pull-right')); ?>
-                                    </div>
-                                    <?php echo form_close(); ?>
                                 </div>
-                            </div>
-                            <div class="tab-pane" id="panel-403986">
-                                <p>
-                                    Howdy, I'm in Section 2.
-                                </p>
+                                <div class="tab-pane" id="panel-403986">
+                                    <div class="row clearfix">
+                                        <div class="col-md-12 column">
+                                            <h3>Update Notes</h3>
+                                            <div class="form-group">
+                                                <?php echo form_textarea('reques_description', '', array('class' => 'form-control', 'placeholder' => 'Post an Update', 'rows' => '3')); ?>
+                                            </div>
+                                            <div class="form-group">
+                                                <?php echo form_submit("save", "Post", array('class' => 'btn btn-primary pull-right')); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
-</div>
 </div>
 <a href="<?php echo site_url('/WorkOrders/add'); ?>" class="btn paper-button paper-floating-action-button">
     <i class="glyphicon glyphicon-plus"></i>
