ac496b63cc3d75e85948c37f8614992c6fde4b2f
zainabzoaib <zainab.zoaib@gmail.com>
2017-03-25 18:07:41 +0500

    edited employees

--- application/views/Employees/add.php
+++ application/views/Employees/add.php
@@ -1,61 +1,63 @@
-
-
-<div class="span12">
-    <div class="panel panel-primary">
-        <div class="panel-body">
-            <h3>Add New People</h3>
-            <div class="row-fluid">
-                <div class="span12">
-                    <div class="form-group">
-                        <?php echo form_label('Email', '', array('class' => 'control-label')); ?>
+<div class="row-fluid">
+    <div class="span12">
+        <div class="widget-box"> 
+            <div class="widget-title"> <span class="icon"> <i class="icon-file"></i> </span>
+                <h5>Add New People</h5>
+            </div>
+            <div class="widget-content">
+                <div class="row-fluid">
+                    <div class="span12">
                         <div class="form-group">
-                            <?php echo form_input('email', '', array('class' => 'form-control', 'placeholder' => 'abc@example.com(will also be the username)*')) ?>
-                        </div>
-                        <div class="form-group">
-                            <?php echo form_label('Password', '', array('class' => 'control-label')); ?>
+                            <?php echo form_label('Email', '', array('class' => 'control-label')); ?>
                             <div class="form-group">
-                                <?php echo form_password('password', '', array('class' => 'form-control', 'placeholder' => 'Password*')) ?>
+                                <?php echo form_input('email', '', array('class' => 'span12', 'placeholder' => 'abc@example.com(will also be the username)*')) ?>
                             </div>
                             <div class="form-group">
-                                <?php echo form_label('User Account Type', 'workorder_priority', array('class' => 'control-label')); ?>
-                                <?php echo form_dropdown('employee_type', array('Select type', 'Admin', 'Manager', 'Employee'), FALSE, array('class' => 'form-control')) ?>
-                            </div>   
-                            <div class="form-group">
-                                <?php echo form_label('First Name', '', array('class' => 'control-label')); ?>
+                                <?php echo form_label('Password', '', array('class' => 'control-label')); ?>
                                 <div class="form-group">
-                                    <?php echo form_input('First_Name', '', array('class' => 'form-control', 'placeholder' => 'First Name*')) ?>
+                                    <?php echo form_password('password', '', array('class' => 'span12', 'placeholder' => 'Password*')) ?>
                                 </div>
                                 <div class="form-group">
-                                    <?php echo form_label('Last Name', '', array('class' => 'control-label')); ?>
+                                    <?php echo form_label('User Account Type', 'workorder_priority', array('class' => 'control-label')); ?>
+                                    <?php echo form_dropdown('employee_type', array('Select type', 'Admin', 'Manager', 'Employee'), FALSE, array('class' => 'span12')) ?>
+                                </div>   
+                                <div class="form-group">
+                                    <?php echo form_label('First Name', '', array('class' => 'control-label')); ?>
                                     <div class="form-group">
-                                        <?php echo form_Input('Last_name', '', array('class' => 'form-control', 'placeholder' => 'Last Name*')) ?>
+                                        <?php echo form_input('First_Name', '', array('class' => 'span12', 'placeholder' => 'First Name*')) ?>
                                     </div>
                                     <div class="form-group">
-                                    <?php echo form_label('Job Tittle', '', array('class' => 'control-label')); ?>
-                                    <div class="form-group">
-                                        <?php echo form_Input('Job_tittle', '', array('class' => 'form-control', 'placeholder' => 'e.g Sales Manager*')) ?>
-                                    </div>
-                                    <div class="form-group">
-                                    <?php echo form_label('Phone Number', '', array('class' => 'control-label')); ?>
-                                    <div class="form-group">
-                                        <?php echo form_input('Phone_number', '', array('class' => 'form-control', 'placeholder' => 'e.g +971 4 xxxxxxx*')) ?>
-                                    </div>
-                                          <div class="form-group">
-                                    <?php echo form_label('Company Name', '', array('class' => 'control-label')); ?>
-                                    <div class="form-group">
-                                        <?php echo form_input('Company_name', '', array('class' => 'form-control', 'placeholder' => 'Company Name*')) ?>
-                                    </div>
-                                        
-                                    <div class="form-group">
-                                        <?php echo form_submit("Save", "Save", array('class' => 'btn btn-primary pull-right')); ?>
-                                    </div>
+                                        <?php echo form_label('Last Name', '', array('class' => 'control-label')); ?>
+                                        <div class="form-group">
+                                            <?php echo form_Input('Last_name', '', array('class' => 'span12', 'placeholder' => 'Last Name*')) ?>
+                                        </div>
+                                        <div class="form-group">
+                                            <?php echo form_label('Job Tittle', '', array('class' => 'control-label')); ?>
+                                            <div class="form-group">
+                                                <?php echo form_Input('Job_tittle', '', array('class' => 'span12', 'placeholder' => 'e.g Sales Manager*')) ?>
+                                            </div>
+                                            <div class="form-group">
+                                                <?php echo form_label('Phone Number', '', array('class' => 'control-label')); ?>
+                                                <div class="form-group">
+                                                    <?php echo form_input('Phone_number', '', array('class' => 'span12', 'placeholder' => 'e.g +971 4 xxxxxxx*')) ?>
+                                                </div>
+                                                <div class="form-group">
+                                                    <?php echo form_label('Company Name', '', array('class' => 'control-label')); ?>
+                                                    <div class="form-group">
+                                                        <?php echo form_input('Company_name', '', array('class' => 'span12', 'placeholder' => 'Company Name*')) ?>
+                                                    </div>
 
+                                                    <div class="form-group">
+                                                        <?php echo form_submit("Save", "Save", array('class' => 'btn btn-primary pull-right')); ?>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php echo form_close(); ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <?php echo form_close(); ?>
-                    </div>
-                </div>
                         </div>
                     </div>
                 </div>
@@ -64,4 +66,3 @@
     </div>
 </div>
 
-                
--- application/views/Employees/edit.php
+++ application/views/Employees/edit.php
@@ -1,139 +1,65 @@
-
-
-<div class="span6">
-    <div class="panel-group">
-        <div class="panel panel-default">
-            <div class="panel-collapse collapse in">
-                <div class="panel-body">
-                    <ul class="timeline list-unstyled">
-                        <li class="timeline-item first-item">
-                            <ul class="list-unstyled tasks-list">
-                                <li class="task-item show-pointer active">
-                                    <div class="task-item-container task-open">
-                                        <div class="media">
-                                            <div class="media-body">
-                                                <h2 class="media-heading">
-                                                    Testing testing...
-                                                </h2>
-                                            </div>
-                                            <div class="media-right">
-                                                <div class="task-status-box">
-                                                    <a href="<?php echo site_url('Employees/edit'); ?>">
-                                                        <span class="status-circle task-open"><i class="fa fa-pencil"></i></span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="task-item show-pointer active">
-                                    <div class="task-item-container task-open">
-                                        <div class="media">
-                                            <div class="media-body">
-                                                <h2 class="media-heading">
-                                                    Testing testing...
-                                                </h2>
-                                            </div>
-                                            <div class="media-right">
-                                                <div class="task-status-box">
-                                                    <a href="<?php echo site_url('Employees/edit'); ?>">
-                                                        <span class="status-circle task-open"><i class="fa fa-pencil"></i></span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="task-item show-pointer active">
-                                    <div class="task-item-container task-open">
-                                        <div class="media">
-                                            <div class="media-body">
-                                                <h2 class="media-heading">
-                                                    Testing testing...
-                                                </h2>
-                                            </div>
-                                            <div class="media-right">
-                                                <div class="task-status-box">
-                                                    <a href="<?php echo site_url('Employees/edit'); ?>">
-                                                        <span class="status-circle task-open"><i class="fa fa-pencil"></i></span>
-                                                    </a>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="task-item show-pointer active">
-                                    <div class="task-item-container task-open">
-                                        <div class="media">
-                                            <div class="media-body">
-                                                <h2 class="media-heading">
-                                                    Testing testing...
-                                                </h2>
-                                            </div>
-                                            <div class="media-right">
-                                                <div class="task-status-box">
-                                                    <a href="<?php echo site_url('Employees/edit'); ?>">
-                                                        <span class="status-circle task-open"><i class="fa fa-pencil"></i></span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+<div class="row-fluid">
+    <div class="span6">
+        <div class="widget-box">
+            <div class="widget-title"> <span class="icon"> <i class="icon-file"></i> </span>
+                <h5>Requests</h5>
             </div>
+            <ul class="recent-posts">
+                <li>
+                    <div class="article-post">
+                        <div class="fr"><a href="<?php echo site_url('Requests/edit/'); ?>" class="btn btn-primary btn-mini">Edit</a></div>
+                        <p> Testing</p>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
-
-</div>
 <div class="span6">
-    <div class="panel panel-primary">
-        <div class="panel-body">
-            <h3>Edit Profiles</h3>
+   <div class="widget-box"> 
+            <div class="widget-title"> <span class="icon"> <i class="icon-file"></i> </span>
+                <h5>Edit Profiles</h5>
+            </div>
+       <div class="widget-content">
             <div class="row-fluid">
                 <div class="span12">
                     <div class="form-group">
                         <?php echo form_label('Email', '', array('class' => 'control-label')); ?>
                         <div class="form-group">
-                            <?php echo form_input('email', '', array('class' => 'form-control', 'placeholder' => 'abc@example.com(will also be the username)*')) ?>
+                            <?php echo form_input('email', '', array('class' => 'span12', 'placeholder' => 'abc@example.com(will also be the username)*')) ?>
                         </div>
                         <div class="form-group">
                             <?php echo form_label('Password', '', array('class' => 'control-label')); ?>
                             <div class="form-group">
-                                <?php echo form_password('password', '', array('class' => 'form-control', 'placeholder' => 'Password*')) ?>
+                                <?php echo form_password('password', '', array('class' => 'span12', 'placeholder' => 'Password*')) ?>
                             </div>
                             <div class="form-group">
                                 <?php echo form_label('User Account Type', 'workorder_priority', array('class' => 'control-label')); ?>
-                                <?php echo form_dropdown('employee_type', array('Select type', 'Admin', 'Manager', 'Employee'), FALSE, array('class' => 'form-control')) ?>
+                                <?php echo form_dropdown('employee_type', array('Select type', 'Admin', 'Manager', 'Employee'), FALSE, array('class' => 'span12')) ?>
                             </div>   
                             <div class="form-group">
                                 <?php echo form_label('First Name', '', array('class' => 'control-label')); ?>
                                 <div class="form-group">
-                                    <?php echo form_input('First_Name', '', array('class' => 'form-control', 'placeholder' => 'First Name*')) ?>
+                                    <?php echo form_input('First_Name', '', array('class' => 'span12', 'placeholder' => 'First Name*')) ?>
                                 </div>
                                 <div class="form-group">
                                     <?php echo form_label('Last Name', '', array('class' => 'control-label')); ?>
                                     <div class="form-group">
-                                        <?php echo form_Input('Last_name', '', array('class' => 'form-control', 'placeholder' => 'Last Name*')) ?>
+                                        <?php echo form_Input('Last_name', '', array('class' => 'span12', 'placeholder' => 'Last Name*')) ?>
                                     </div>
                                     <div class="form-group">
                                         <?php echo form_label('Job Tittle', '', array('class' => 'control-label')); ?>
                                         <div class="form-group">
-                                            <?php echo form_Input('Job_tittle', '', array('class' => 'form-control', 'placeholder' => 'e.g Sales Manager*')) ?>
+                                            <?php echo form_Input('Job_tittle', '', array('class' => 'span12', 'placeholder' => 'e.g Sales Manager*')) ?>
                                         </div>
                                         <div class="form-group">
                                             <?php echo form_label('Phone Number', '', array('class' => 'control-label')); ?>
                                             <div class="form-group">
-                                                <?php echo form_input('Phone_number', '', array('class' => 'form-control', 'placeholder' => 'e.g +971 4 xxxxxxx*')) ?>
+                                                <?php echo form_input('Phone_number', '', array('class' => 'span12', 'placeholder' => 'e.g +971 4 xxxxxxx*')) ?>
                                             </div>
                                             <div class="form-group">
                                                 <?php echo form_label('Company Name', '', array('class' => 'control-label')); ?>
                                                 <div class="form-group">
-                                                    <?php echo form_input('Company_name', '', array('class' => 'form-control', 'placeholder' => 'Company Name*')) ?>
+                                                    <?php echo form_input('Company_name', '', array('class' => 'span12', 'placeholder' => 'Company Name*')) ?>
                                                 </div>
 
                                                 <div class="form-group">
